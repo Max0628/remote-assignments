16de3d76bd386a9a5fcd61a5d043c91045009016
Maxchauo <maxchauo0628@gmail.com>
2024-07-07 23:16:41 +0800

    no chinese comment

--- Week-3/Assignment-5/twoSum.js
+++ Week-3/Assignment-5/twoSum.js
@@ -8,12 +8,12 @@ function twoSum(nums, target) {
     let gap = target - nums[i];
     //if the number is in the obj
     if (gap in obj) {
-      //return the value(index) of gap,and i ,"i" lterate the array earlier
+      //return the value(index) of gap,and i ,"i" iterate the array earlier
       return [obj[gap], i];
     }
     obj[nums[i]] = i; //insert can't in front of if statement!!
   }
   return -1;
 }
-console.log(twoSum([2, 7, 11, 15], 9)); // 應該返回 [0, 1]
-//console.log(twoSum([7, 7, 11, 15, 1, 8, 3, 6, 4, 4], 16)); // 應該返回 [3,4]
+console.log(twoSum([2, 7, 11, 15], 9)); // return [0, 1]
+//console.log(twoSum([7, 7, 11, 15, 1, 8, 3, 6, 4, 4], 16)); // return  [3,4]
