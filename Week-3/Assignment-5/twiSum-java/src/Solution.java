66c052bedb8eb1acebbedf82be602ccfafe06d1b
Maxchauo <maxchauo0628@gmail.com>
2024-07-07 20:03:55 +0800

    move insert before if statement

---
+++ Week-3/Assignment-5/twiSum-java/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+public class Test {
+    public static int[] twoSum(int[] nums, int target) {
+
+        //use Hashmap to store numbers as key,index as value
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i =0 ;i<nums.length;i++){
+            //find the numbers we need
+            int gap = target - nums[i];
+
+            //if hashMap match the number we loop
+            if(map.containsKey(nums[i])){
+
+                //return a new array,get the old number's index and new number's index
+                return  new int[]{map.get(nums[i]),i};
+            }
+
+            //if not contain this number,maybe next time will match,store it into hashMap
+            map.put(gap,i);
+
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
--- Week-3/Assignment-5/twoSum.js
+++ Week-3/Assignment-5/twoSum.js
@@ -6,17 +6,14 @@ function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     //this number maybe can match
     let gap = target - nums[i];
-
-    //store num[i] as key,index as value,that is convenient for match return index
-    obj[nums[i]] = i;
-
     //if the number is in the obj
     if (gap in obj) {
       //return the value(index) of gap,and i ,"i" lterate the array earlier
       return [obj[gap], i];
     }
+    obj[nums[i]] = i; //insert can't in front of if statement!!
   }
   return -1;
 }
-//console.log(twoSum([2, 7, 11, 15], 9)); // 應該返回 [0, 1]
+console.log(twoSum([2, 7, 11, 15], 9)); // 應該返回 [0, 1]
 //console.log(twoSum([7, 7, 11, 15, 1, 8, 3, 6, 4, 4], 16)); // 應該返回 [3,4]
